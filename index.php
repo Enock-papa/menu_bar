527e2763681267a5f4333754375aca32b3b967dc
Enock-papa <94233106+Enock-papa@users.noreply.github.com>
2022-05-10 03:15:19 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>menu-bar</title>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css" integrity="sha384-jLKHWM3JRmfMU0A5x5AkjWkw/EYfGUAGagvnfryNV3F9VqM98XiIH7VBGVoxVSc7" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main_box">
+		<input type="checkbox" id="check">
+		<div class="btn_one">
+			<label for="check">
+				<i class="fa fa-bars"></i>
+			</label>
+		</div>
+		<div class="sidebar_menu">
+			<div class="logo">
+		<a href="#">Sidebar</a>
+	</div>
+	<div class="btn_two">
+			<label for="check">
+				<i class="fa fa-times"></i>
+			</label>
+		</div>
+	<div class="menu">
+	<ul>
+		<li><a href="#">
+			<i class="fa fa-qrcode"></i>Dashboard</a></li>
+		
+		<li><a href="#"><i class="fa fa-link"></i>
+		Shortcut</a></li>
+		
+		<li><a href="#"><i class="fa fa-camera"></i>
+		Overview</a></li>
+		
+		<li><a href="#"><i class="fa fa-calendar"></i>
+		Events</a></li>
+		
+		<li><a href="#"><i class="fa fa-question-circle"></i>
+		About</a></li>
+		
+		<li><a href="#"><i class="fa fa-sliders-h"></i>
+		Services</a></li>
+		
+		<li><a href="#"><i class="fa fa-phone-volume"></i>
+		Contacts</a></li>
+		
+		<li><a href="#"><i class="fa fa-comments "></i>
+		Feedback</a></li>
+
+		
+	</ul>
+</div>
+<div class="social_media">
+	<a href="#"><i class="fa fa-facebook"></i></a>
+	<a href="#"><i class="fa fa-twitter"></i></a>
+	<a href="#"><i class="fa fa-instagram"></i></a>
+	<a href="#"><i class="fa fa-youtube"></i></a>
+
+
+</div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
